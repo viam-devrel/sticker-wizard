b39c94e16e28ce712c8d7d073dc3050d0dbd2f7e
emily hong <emily.hong@viam.com>
2024-09-20 17:08:21 -0400

    fix motor calibration

--- src/lib/machine.ts
+++ src/lib/machine.ts
@@ -9,10 +9,60 @@ export interface Machine {
 	dispenseSticker: (result: number) => Promise<void>;
 }
 
-const MOTOR_TO_PIN = ['0', '1', '2', '4', '5', '6', '8', '9', '10'];
-const MOTOR_TO_DISPENSE_TIME_MS = [1_400, 1_700, 1_700, 1_600, 1_400, 1_600, 1_300, 1_600, 1_725];
+interface MotorBoardConfig {
+	pin: number;
+	dispenseTime: number;
+	dutyCycle: number;
+}
+
+const MOTOR_TO_CONFIG: MotorBoardConfig[] = [
+	{
+		pin: 0,
+		dispenseTime: 1_750,
+		dutyCycle: 0.075
+	},
+	{
+		pin: 1,
+		dispenseTime: 1_900,
+		dutyCycle: 0.075
+	},
+	{
+		pin: 2,
+		dispenseTime: 1_700,
+		dutyCycle: 0.09
+	},
+	{
+		pin: 4,
+		dispenseTime: 1_900,
+		dutyCycle: 0.075
+	},
+	{
+		pin: 5,
+		dispenseTime: 1_700,
+		dutyCycle: 0.09
+	},
+	{
+		pin: 6,
+		dispenseTime: 2_100,
+		dutyCycle: 0.075
+	},
+	{
+		pin: 8,
+		dispenseTime: 1_300,
+		dutyCycle: 0.09
+	},
+	{
+		pin: 9,
+		dispenseTime: 1_600,
+		dutyCycle: 0.09
+	},
+	{
+		pin: 10,
+		dispenseTime: 2_500,
+		dutyCycle: 0.075
+	}
+];
 const MOTOR_FREQ = 50;
-const ON_DUTY_CYCLE = 0.09;
 
 export const createMachine = async (): Promise<Machine> => {
 	const machine = await VIAM.createRobotClient({
@@ -28,13 +78,13 @@ export const createMachine = async (): Promise<Machine> => {
 	const dispenseSticker = async (result: number) => {
 		console.log('dispensing sticker...');
 
-		const pin = MOTOR_TO_PIN[result];
+		const { pin, dispenseTime, dutyCycle } = MOTOR_TO_CONFIG[result];
 		const motorBoardClient = new VIAM.BoardClient(machine, 'motor-board');
-		await motorBoardClient.setPWMFrequency(pin, MOTOR_FREQ);
-		await motorBoardClient.setPWM(pin, ON_DUTY_CYCLE);
+		await motorBoardClient.setPWMFrequency(`${pin}`, MOTOR_FREQ);
+		await motorBoardClient.setPWM(`${pin}`, dutyCycle);
 		await setTimeout(async () => {
-			await motorBoardClient.setGPIO(pin, false);
-		}, MOTOR_TO_DISPENSE_TIME_MS[result]);
+			await motorBoardClient.setGPIO(`${pin}`, false);
+		}, dispenseTime);
 	};
 
 	return {
