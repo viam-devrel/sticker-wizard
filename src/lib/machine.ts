379683143782af7d1dd3a38951f22f9b66fa5f1c
emily hong <emily.hong@viam.com>
2024-10-25 16:21:48 -0400

    update typescript SDK

--- src/lib/machine.ts
+++ src/lib/machine.ts
@@ -68,11 +68,11 @@ const MOTOR_FREQ = 50;
 export const createMachine = async (): Promise<Machine> => {
 	const machine = await VIAM.createRobotClient({
 		host: PUBLIC_MACHINE_HOST,
-		credential: {
+		credentials: {
 			type: 'api-key',
-			payload: PUBLIC_MACHINE_API_KEY
+			payload: PUBLIC_MACHINE_API_KEY,
+			authEntity: PUBLIC_MACHINE_API_KEY_ID
 		},
-		authEntity: PUBLIC_MACHINE_API_KEY_ID,
 		signalingAddress: 'https://app.viam.com:443'
 	});
 
